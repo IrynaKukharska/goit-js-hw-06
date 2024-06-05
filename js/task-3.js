266e4ba3fe63fd751326baaf6ce88759ea95048b
Iryna Kukharska <iryna.kukharska20@gmail.com>
2024-06-05 21:07:53 +0200

    changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,15 @@
-"use strict";
-
-function getElementWidth(content, padding, border) {
-  //  1. перетворити рядки на числа за допомогою методу parseFloat або parseInt
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-  // 2. ширину елементу = ширина контенту + верхній і нижній відступи * 2 (падінг*2) + бордер*2
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+function filterArray(numbers, value) {
+  const emptyArray = [];
+  for (const number of numbers) {
+    if (number > value) {
+      emptyArray.push(number);
+    }
+  }
+  return emptyArray;
 }
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
